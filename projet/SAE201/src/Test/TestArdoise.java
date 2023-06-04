4e392d8bd0fbf607cf261159e143dd77dcd75f4e
Melvyn Pommier <melvynlpp@gmail.com>
2023-06-04 20:14:26 +0200

    quadrilateres

--- projet/SAE201/src/Test/TestArdoise.java
+++ projet/SAE201/src/Test/TestArdoise.java
@@ -1,5 +1,8 @@
 package Test;
-import ardoise.*;
+import java.util.ArrayList;
+
+import Main.Quadrilateres;
+import ardoise.*; 
 
 public class TestArdoise {
 
@@ -8,6 +11,24 @@ public class TestArdoise {
 		
 		Ardoise ardoise = new Ardoise();
 		
+		//Tour
+		PointPlan p1 = new PointPlan(9,198);
+		PointPlan p2 = new PointPlan(20,100);
+		Forme tour = new Quadrilateres("tour",p1,p2);
+
+		//Corps Maison
+		PointPlan p3 = new PointPlan(80,198);
+		PointPlan p4 = new PointPlan(180,140);
+		Forme corpsMaison = new Quadrilateres("Corps Maison", p3, p4);
+		
+		//Porte Maison 
+		PointPlan p5 = new PointPlan(120,198);
+		PointPlan p6 = new PointPlan(140,170);
+		Forme porteMaison = new Quadrilateres("Porte Maison", p5, p6);
+		
+		ardoise.ajouterForme(tour);
+		ardoise.ajouterForme(corpsMaison); 
+		ardoise.ajouterForme(porteMaison); 
 		ardoise.test();
 		
 	}
