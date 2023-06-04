64ba5a561187f1c3d17b747c4f2a78849fd1b68b
Melvyn Pommier <melvynlpp@gmail.com>
2023-06-04 18:41:29 +0200

    fichier

---
+++ projet/SAE201/src/Main/Triangle.java
@@ -0,0 +1,33 @@
+package Main;
+
+import java.util.ArrayList;
+
+import ardoise.Forme;
+import ardoise.Segment;
+
+public class Triangle extends Forme 
+{
+
+	public Triangle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void deplacer(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Segment> dessiner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String typeForme() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
