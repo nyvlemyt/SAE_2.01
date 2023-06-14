54322007785936d7da95b39f04cac75709900c49
Johann <johannzidee2004@gmail.com>
2023-06-14 20:47:28 +0200

    maison

--- projet/SAE201/src/Test/TestMaison.java
+++ projet/SAE201/src/Test/TestMaison.java
@@ -1,13 +1,36 @@
 package Test;
 
+import Main.*;
+import ardoise.*;
+
 public class TestMaison {
 
-	public TestMaison() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Corps Maison
+		PointPlan p3 = new PointPlan(80,198);
+		PointPlan p4 = new PointPlan(180,140);
+		Forme corpsMaison = new Quadrilateres("Corps Maison", p3, p4);
+				
+				//Porte Maison 	
+		PointPlan p5 = new PointPlan(120,198);
+		PointPlan p6 = new PointPlan(140,170);
+		Forme porteMaison = new Quadrilateres("Porte Maison", p5, p6);
+				
+				//Toit maison
+		PointPlan p13 = new PointPlan(80,140);
+		PointPlan p14 = new PointPlan(130,100);
+		PointPlan p15 = new PointPlan(180,140);
+		Forme toitMaison = new Chapeaux("Toit maison", p13, p14, p15) ;
+		
+		
+		Ardoise ardoise = new Ardoise();
+		
+		Maison vide = new Maison();
+		Maison house = new Maison(null, porteMaison, corpsMaison, toitMaison);
+		Forme copie1 = new Maison(house); 
+		Forme copie2 = new Maison(copie1); 
+		Forme copie3 = new Maison(copie2); 
 
 	}
 
